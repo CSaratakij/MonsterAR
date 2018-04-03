2a536e779cd010e11055091a8d2abd7a0386c197
Chatchai Saratakij <CSaratakij@gmail.com>
2018-04-04 02:59:51 +0700

    Added Monster model.

---
+++ MonsterAR/Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonsterAR
+{
+    public class GameController : MonoBehaviour
+    {
+        public static GameController instance;
+
+        public static event Func OnGameStart;
+        public static event Func OnGameOver;
+
+        public delegate void Func();
+
+
+        bool isGameStart;
+        bool isGameOver;
+
+
+        void Awake()
+        {
+            _Initialize();
+        }
+
+        void _Initialize()
+        {
+            if (instance == null) {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else {
+                Destroy(gameObject);
+            }
+        }
+
+        void _FireEvent_OnGameStart()
+        {
+            if (OnGameStart != null) {
+                OnGameStart();
+            }
+        }
+
+        void _FireEvent_OnGameOver()
+        {
+            if (OnGameOver != null) {
+                OnGameOver();
+            }
+        }
+    }
+}
